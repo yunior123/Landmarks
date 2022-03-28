7ac6474cce0576d3e0cdf73cf2e54e75ad75ec94
Yunior Rodriguez <47259501+yunior123@users.noreply.github.com>
2022-03-28 03:18:21 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Yunior Rodriguez Osorio on 28/3/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
