faab4273a2cad43aeb04653288ddfdeac74ab828
Yunior Rodriguez <47259501+yunior123@users.noreply.github.com>
2022-03-28 04:45:49 -0400

    1-Creating and combining views

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Yunior Rodriguez Osorio on 28/3/22.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                         Circle().stroke(.white, lineWidth: 4)
+                     }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,8 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            MapView()
+                           .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                                .padding(.bottom, -130)
+            VStack (
+                alignment: .leading,
+                spacing: 10
+            ) {
+             
+                Text("Turtle Rock!").font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                        
+                } .font(.subheadline)
+                    .foregroundColor(.secondary)
+                Divider()
+
+                              Text("About Turtle Rock")
+                                  .font(.title2)
+                              Text("Descriptive text goes here.")
+        }.padding()
+            Spacer()
+            
+        }
+            
     }
 }
 
