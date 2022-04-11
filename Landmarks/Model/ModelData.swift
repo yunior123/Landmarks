040cdb33e148562828294d32d40d2e561b5dde0f
Yunior Rodriguez <47259501+yunior123@users.noreply.github.com>
2022-04-11 10:06:41 -0400

    7- UI controllers

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -12,6 +12,7 @@ import Combine
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+    @Published var profile = Profile.default
     var categories: [String: [Landmark]] {
           Dictionary(
               grouping: landmarks,
